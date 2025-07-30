c9e712d393cc6145679bd562b999842bde976e33
Dulani-JK <dulanijayakodi8@gmail.com>
2025-07-30 16:05:53 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~xudzjRTI#D5NRFtnOMOog9M9OE0JbMr8wWoKEcNbQYwTXinQ0eSY",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "94776338644", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
